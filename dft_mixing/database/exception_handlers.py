89be4371e2885fcbd54232d29fc24e4f79413b16
Kenrick-MH <kenrickmarcellhaditio@gmail.com>
2025-08-12 16:41:59 -0700

    add more dashcoeff aliases

--- dft_mixing/database/exception_handlers.py
+++ dft_mixing/database/exception_handlers.py
@@ -7,6 +7,23 @@ missing_parent_functionals = [
         "xc_functionals": {
             "GGA_XC_B97_D": {}
         }
+    },
+    
+    {
+        "name": "mp2",
+        "x_hf": {
+            "alpha": 1.0
+        },
+        "c_functionals": {},
+        "c_mp2": {"alpha": 1.0}, 
+        'citation': '    Rezac, J.; Greenwell, C.; Beran, G. (2018), J. Chem. Theory Comput., 14: 4711-4721\n'
+    },
+    
+    {
+        "name": "b97_3c",
+        "xc_functionals": {
+            "GGA_XC_B97_3C": {}
+        }
     }, 
     
     {
@@ -25,6 +42,13 @@ missing_parent_functionals = [
     
     {
         "name": "opbe_d",
+        "xc_functionals": {
+            "GGA_XC_OPBE_D": {}
+        }
+    }, 
+    
+    {
+        "name": "otpss_d",
         "xc_functionals": {
             "GGA_XC_OPBE_D": {}
         }
@@ -90,10 +114,18 @@ additional_aliases = {
     "b97_2": "b97-2",
     "b98": "sb98-2c", 
     "b97d": "b97_d",
-    
-    
-    
-    
+}
+
+dashcoeff_mapping = {
+    "b97-3c": ("b97_3c", "d3bjatm"),
+    "b973c": ("b97_3c", "d3bjatm"),
+    "r2scan3c": ("r2scan", "d4bjeeqatm"), 
+    "hf+d": ("hf", "das2010"),
+    "hf3c": ("hf", "d3bj2b"),
+    "pbeh3c": ("pbe0", "d3bj2b"),
+    "dldf+d09": ("dldf", "das2009"),
+    "dldf+d10": ("dldf", "das2010"),
+    "mp2d": ("mp2", "dmp2")
 }
 
 
